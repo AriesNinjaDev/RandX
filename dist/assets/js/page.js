51e7fc14097cd493ffce4e8957ba5e35d92d2fdf
Aries Powvalla <74034072+AriesNinjaDev@users.noreply.github.com>
2024-03-21 21:26:41 -0700

    checkpoint 7

--- dist/assets/js/page.js
+++ dist/assets/js/page.js
@@ -26,7 +26,7 @@ function execute() {
         document
             .querySelectorAll(".CodeMirror-line")
             .forEach(function (element) {
-                element.style.color = "red";
+                element.style.color = "blue";
             });
         return;
     }
@@ -62,3 +62,65 @@ document.addEventListener("keydown", function (event) {
         execute();
     }
 });
+
+var saveData = (function () {
+    var a = document.createElement("a");
+    return function (textData, fileName) {
+        var blob = new Blob([textData], { type: "text/plain" }), // Specify text/plain type for text data
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+})();
+
+var importData = function (callback) {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".rscript";
+
+    input.onchange = function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function () {
+            var fileContent = reader.result;
+            callback(fileContent);
+        };
+
+        reader.readAsText(file);
+    };
+
+    input.click();
+};
+
+function saveScriptWithTimestamp(scriptContent, filenamePrefix) {
+    var currentDate = new Date();
+    var dateString = currentDate
+        .toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        })
+        .replace(/\//g, "-");
+    var timeString = currentDate
+        .toLocaleTimeString("en-US", { hour12: false })
+        .replace(/:/g, "-");
+    var filename =
+        filenamePrefix + "_" + dateString + "_" + timeString + ".rscript";
+
+    saveData(scriptContent, filename);
+}
+
+function doExport() {
+    saveScriptWithTimestamp(editor.getValue(), "script");
+    alertify.success("Script exported successfully.");
+}
+
+function doImport() {
+    importData(function (fileContent) {
+        editor.getDoc().setValue(fileContent);
+        alertify.success("Script imported successfully.");
+    });
+}
